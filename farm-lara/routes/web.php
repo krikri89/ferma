3d5d1931fba726df177292c74dd3b2289d0319e5
Kristina Rudyte <rkristina32@yahoo.com>
2022-07-11 22:12:31 +0300

    edit and delete fixed

--- farm-lara/routes/web.php
+++ farm-lara/routes/web.php
@@ -21,6 +21,6 @@ Route::get('/', function () {
 Route::get('/animals', [F::class, 'index'])->name('animals-index');
 Route::get('/animals/create', [F::class, 'create'])->name('animals-create');
 Route::post('/animals', [F::class, 'store'])->name('animals-store');
-Route::get('/animals/edit/{id}', [F::class, 'edit'])->name('animals-edit');
-Route::put('/animals/{id}', [F::class, 'update'])->name('animals-update');
-Route::delete('/animals/{id}', [F::class, 'destroy'])->name('animals-delete');
+Route::get('/animals/edit/{farm}', [F::class, 'edit'])->name('animals-edit');
+Route::put('/animals/{farm}', [F::class, 'update'])->name('animals-update');
+Route::delete('/animals/{farm}', [F::class, 'destroy'])->name('animals-delete');
